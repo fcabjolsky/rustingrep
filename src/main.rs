a0bb76d9925af849e597a875b3d04bb4e8bdad8c
Felipe <felipe@frontm.com>
2023-04-09 18:32:34 -0300

    refactoring: split into mods

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,16 @@
-use std::collections::HashSet;
+mod search_engine;
+use search_engine::SearchEng;
 
 use clap::Parser;
-use regex::Regex;
 
 //TODO: GREP into the file directory
 
 #[derive(Debug, Clone)]
-struct Options {
+pub struct Options {
     ctx_lines: usize,
     print_line_numbers: bool,
 }
 
-#[derive(Debug, Clone)]
-struct SearchEng {
-    source: Vec<String>,
-    opts: Options,
-    tags: HashSet<usize>,
-}
-
-impl SearchEng {
-    /// creates a new seaerch engine for the file_path with options
-    fn new_from_file(file_path: &str, options: Options) -> Self {
-        let file = std::fs::read_to_string(file_path).expect("can't read the file");
-        let content: Vec<String> = file.split("\n").map(str::to_string).collect();
-        SearchEng {
-            source: content,
-            opts: options,
-            tags: HashSet::new(),
-        }
-    }
-
-    /// searches the pattern to_search in the current search engine 
-    fn search(&mut self, to_search: &str) {
-        let pattern = Regex::new(to_search).expect("can't create the patter");
-
-        for (i, line) in self.source.iter().enumerate() {
-            if let Some(_) = pattern.find(line) {
-                    self.tags.insert(i);
-            }
-        }
-
-    }
-    
-    /// prints the resulsts found by the search engine
-    fn print_results(&self) {
-        let mut tags: Vec<_> = self.tags.iter().collect();
-        tags.sort();
-
-        for tag in tags {
-            let lower_ctx = (*tag).saturating_sub(self.opts.ctx_lines);
-            let upper_ctz = std::cmp::min(*tag + self.opts.ctx_lines, self.source.len() - 1);
-
-            for i in lower_ctx..=upper_ctz {
-                let mut prefix = "";
-                let mut line_number = "".to_string();
-                if i == *tag {
-                    prefix = ">>";
-                }
-                if self.opts.print_line_numbers {
-                    line_number = (i + 1).to_string();
-
-                }
-
-                println!("{} {} {}", prefix, line_number, self.source[i]);
-            }
-            println!("--------");
-        }
-
-    }
-}
 
 /// searches for patterns
 #[derive(Parser, Debug)]
